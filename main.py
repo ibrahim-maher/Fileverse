b88d4ed36055d0f5ec4359f3b18e98f9b2356644
AhmedAbo6 <ahmed.abo.sita66@gmail.com>
2021-11-01 06:25:11 +0200

    Starter window

---
+++ main.py
@@ -0,0 +1,21 @@
+import sqlite3
+from tkinter import *
+
+
+# The main window
+main = Tk()
+main.title('Fileverse') # title
+main.iconbitmap("img/am6-logo.ico") # icon
+main.geometry('300x320') # area
+# Make the window unresizable
+main.resizable(0, 0)
+
+
+
+
+
+
+
+
+# maitianing the app open
+main.mainloop()
